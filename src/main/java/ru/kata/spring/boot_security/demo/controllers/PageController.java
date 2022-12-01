207d507ff808a1a09367e580ea5d9ba4f4605676
Sergei <395105@mail.ru>
2022-12-01 16:53:31 +0400

    - bug fix

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/PageController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/PageController.java
@@ -4,15 +4,9 @@ package ru.kata.spring.boot_security.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-import ru.kata.spring.boot_security.demo.services.UserService;
 
 @Controller
 public class PageController {
-    private final UserService userService;
-
-    public PageController(UserService userService) {
-        this.userService = userService;
-    }
     @GetMapping("/")
     public String MainPage() {
         return "redirect:/login";
